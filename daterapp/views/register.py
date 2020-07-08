1f5f2530b8247490934efc40a7472bc33130f741
Kurt Krafft <kkrafft6609@gmail.com>
2020-07-08 11:55:42 -0500

    Wooo got user specific dates to get returned

--- daterapp/views/register.py
+++ daterapp/views/register.py
@@ -3,24 +3,7 @@ import json
 from django.http import HttpResponse
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
-
-
-config = {
-    "apiKey": "AIzaSyCzvwzt_1Hq7n10rEVBAZ0dmHtsz0_4CrI",
-    "authDomain": "dater-385dc.firebaseapp.com",
-    "databaseURL": "https://dater-385dc.firebaseio.com",
-    "projectId": "dater-385dc",
-    "storageBucket": "dater-385dc.appspot.com",
-    "messagingSenderId": "327978469990",
-    "appId": "1:327978469990:web:f3eb2cc59f544d84e9bba7",
-    "measurementId": "G-L06SK2997T"
-  }
-
-
-
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-db = firebase.database()
+from .firebase  import auth, firebase, db 
 
 @csrf_exempt
 def register_user(request):
@@ -41,7 +24,7 @@ def register_user(request):
 
         uid = user['localId']
 
-        data={"name":name, "birthdate": birthdate}
+        data={"name":name, "birthdate": birthdate, "status": "1"}
 
         db.child('users').child(uid).child("details").set(data)
 
