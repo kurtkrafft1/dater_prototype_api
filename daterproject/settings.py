85628b34b20c2adbb89edda1a979e9a907efb390
Kurt Krafft <kkrafft6609@gmail.com>
2020-07-10 16:14:43 -0500

    cors issues

--- daterproject/settings.py
+++ daterproject/settings.py
@@ -88,10 +88,24 @@ WSGI_APPLICATION = 'daterproject.wsgi.application'
 
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
     'http://localhost:3000',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:3000',
+
 )
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'token',
+]
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
